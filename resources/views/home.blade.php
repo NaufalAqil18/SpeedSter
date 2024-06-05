36c2d1a1979f26b38a257c9b20ea6c71ed931c19
NaufalAqil18 <naufal.aq@mhs.usk.ac.id>
2024-06-05 12:15:13 +0700

    menyelesaikan styling tahap pertama terhadap website

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,8 +27,7 @@
                     <div class="topic">TES KECEPATAN MENGETIK</div>
                     <div class="des">
                         <!-- lorem 50 -->
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
+                        Dalam dunia yang semakin cepat berputar, kecepatan mengetik bukan lagi sekadar keterampilan, melainkan suatu keharusan. Speedster hadir sebagai mitra Anda dalam meraih tingkat kecepatan mengetik yang luar biasa, memberikan tantangan yang memacu adrenalin dan melatih ketangkasan jari-jari Anda. Segera bergabung dengan komunitas kami dan jelajahi kemampuan mengetik Anda melalui uji coba yang menantang, sambil berkompetisi dengan waktu yang terus berlari. Dengan Speedster, setiap ketukan keyboard membawa Anda lebih dekat pada penguasaan total atas dunia mengetik. Bersiaplah untuk melampaui batas-batas diri dan menjadi juara dalam dunia mengetik!                     </div>
                 </div>
             </div>
             <div class="item">
@@ -38,8 +37,7 @@
                     <div class="title">SPEEDSTER</div>
                     <div class="topic">BERPACU DENGAN WAKTU</div>
                     <div class="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
+                        Dalam era di mana waktu adalah komoditas berharga, Speedster hadir sebagai sahabat Anda dalam berpacu dengan detik. Nikmati pengalaman uji cepat yang menggetarkan hati, di mana Anda berlomba melawan waktu untuk meningkatkan ketangkasan mengetik Anda. Setiap detik terasa berharga ketika Anda menekan tombol-tombol keyboard dengan gesit, mengikuti ritme ketukan waktu yang tak kenal lelah. Dengan Speedster, kecepatan dan ketepatan Anda diuji dalam sebuah perjalanan cepat menuju penguasaan penuh atas teknik mengetik. Bersiaplah, waktu tidak akan menunggu siapapun. Ayo berpacu bersama Speedster dan menjadi juara dalam dunia mengetik!                    </div>
                 </div>
             </div>
             <div class="item">
@@ -49,8 +47,7 @@
                     <div class="title">SPEEDSTER</div>
                     <div class="topic">BERPACU DENGAN BANYAK KATA</div>
                     <div class="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
+                        Dalam perjalanan yang tak kenal lelah menuju penguasaan kata-kata, Speedster hadir sebagai pengemudi penuh semangat. Di dalam dunia di mana setiap huruf memiliki arti yang tak ternilai, kami mempersembahkan tantangan untuk berpacu dengan keberanian dan kecerdasan. Segera bergabunglah dengan komunitas kami yang penuh semangat, di mana Anda akan menemukan bahwa setiap kalimat adalah rintangan yang harus diatasi dengan kecepatan dan ketepatan. Mari kita berlomba dengan kata-kata, melangkah dengan tekad yang tak tergoyahkan menuju puncak keahlian mengetik. Bersiaplah untuk menemukan potensi tersembunyi dalam setiap jari-jari Anda dan menjadi ahli dalam dunia mengetik!                     </div>
                 </div>
             </div>
             <div class="item">
@@ -60,8 +57,7 @@
                     <div class="title">SPEEDSTER</div>
                     <div class="topic">BERSAING DENGAN YANG LAIN</div>
                     <div class="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
+                        Dalam dunia yang penuh dengan persaingan, Speedster hadir sebagai teman setia yang siap membawa Anda menuju kemenangan. Di tengah riuh rendah kompetisi, kami mengundang Anda untuk melangkah maju dan membuktikan keunggulan Anda dalam kecepatan dan ketepatan mengetik. Bergabunglah dengan jutaan peserta lainnya dan rasakan sensasi ketegangan saat Anda berpacu dengan waktu, mencoba untuk mengalahkan rekor dan meraih gelar juara. Segera bersiaplah, karena di dalam arena Speedster, setiap detik berharga dan setiap kecepatan hitungan. Mari bersaing dengan semangat yang menggebu-gebu, dan jadilah yang terdepan dalam dunia mengetik!                    </div>
                 </div>
             </div>
         </div>
@@ -73,9 +69,6 @@
                     <div class="title">
                         SPEEDSTER
                     </div>
-                    <div class="description">
-                        Description
-                    </div>
                 </div>
             </div>
             <div class="item">
@@ -83,9 +76,6 @@
                 <div class="content">
                     <div class="title">
                         WAKTU
-                    </div>
-                    <div class="description">
-                        Description
                     </div>
                 </div>
             </div>
@@ -95,9 +85,6 @@
                     <div class="title">
                         KATA
                     </div>
-                    <div class="description">
-                        Description
-                    </div>
                 </div>
             </div>
             <div class="item">
@@ -105,9 +92,6 @@
                 <div class="content">
                     <div class="title">
                         BERSAING
-                    </div>
-                    <div class="description">
-                        Description
                     </div>
                 </div>
             </div>
